6ab5463704bf95455ac9d31326c84a3860c260c9
haykbit <hpetrosyan.projects@gmail.com>
2021-09-20 21:19:57 +0200

    Style: navbar structure

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,9 +1,10 @@
-import "../../styles/colors.scss";
+import "../../styles/navbar/navbar.scss";
 
 function Navbar() {
   return (
   <div className="nav-container">
       <div className="search">
+          <div className="circle"></div>
           <form>
               <input type="text" placeholder="Search..."/>
           </form>
