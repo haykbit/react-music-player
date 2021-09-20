b42a269c2b4592fb18d61d2d71d53f48aaaae7ae
haykbit <hpetrosyan.projects@gmail.com>
2021-09-21 01:39:59 +0200

    style: navbar structure added

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,6 +1,9 @@
 import { RiSettings4Line, RiMusic2Line, RiHistoryFill } from "react-icons/ri";
 import { MdFavoriteBorder } from "react-icons/md";
 import { GoListUnordered } from "react-icons/go";
+import { FaUserCircle } from "react-icons/fa";
+import userImage from "../../assets/images/profile.jpg";
+
 import "./style/navbar.scss";
 
 function Navbar() {
@@ -16,26 +19,41 @@ function Navbar() {
       <div className="nav-rigth-side">
         <div className="user">
           <input type="checkbox" id="menu" />
-          <label for="menu" class="icon">
+          <label for="menu" className="icon">
             <div className="menu"></div>
           </label>
 
           <nav>
             <ul>
+              <li className="user-icon-li">
+                <div
+                  className="user-icon"
+                  style={{
+                    backgroundImage: `url(${userImage})`,
+                    backgroundSize: "contain",
+                  }}
+                ></div>
+                <h5>Laura Morales</h5>
+              </li>
               <li>
                 <RiMusic2Line className="list-icon" />
+                <h4>Playlists</h4>
               </li>
               <li>
                 <MdFavoriteBorder className="list-icon" />
+                <h4>Favorit</h4>
               </li>
               <li>
                 <GoListUnordered className="list-icon" />
+                <h4>General list</h4>
               </li>
               <li>
                 <RiHistoryFill className="list-icon" />
+                <h4>History</h4>
               </li>
               <li>
                 <RiSettings4Line className="list-icon" />
+                <h4>Settings</h4>
               </li>
             </ul>
           </nav>
