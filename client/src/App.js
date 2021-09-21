b60f3de34fee73860cebea098bee87032be5af41
Sebastian Brupbacher <sebastian.eliasb@gmail.com>
2021-09-21 12:18:07 +0200

    style: Advancing with registerForm

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar/";
-//import Register from "./Pages/Register/";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div className="App">
-      {/*<Register /> */}
-      <Navbar />
+      <Register />
+      {/* <Navbar /> */}
     </div>
   );
 }
