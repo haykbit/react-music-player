d5beae646a8965217d1b9fc8cb8775b36b967e86
김은영 <solaz0824@gmail.com>
2021-09-21 12:02:24 +0200

    feat: google firebase functions added and index file created

--- client/src/services/auth/auth.js
+++ client/src/services/auth/auth.js
@@ -16,3 +16,47 @@ if (!firebase.apps.length) {
   // if already initialized, use that one
   firebase.app();
 }
+
+export const auth = firebase.auth();
+
+export function onAuthStateChanged(...props) {
+  return auth.onAuthStateChanged(...props);
+}
+
+export function signInWithGoogle() {
+  const GoogleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+  return auth.signInWithPopup(GoogleAuthProvider);
+}
+
+export function signInWithEmailAndPassword(email, password) {
+  return auth.signInWithEmailAndPassword(email, password);
+}
+
+export function signUpWithEmailAndPassword(email, password) {
+  return auth.createUserWithEmailAndPassword(email, password);
+}
+
+export function sendPasswordResetEmail(email) {
+  return auth.sendPasswordResetEmail(email);
+}
+
+export function signOut() {
+  return auth.signOut();
+}
+
+export function getCurrentUserToken() {
+  if (!auth.currentUser) {
+    return null;
+  }
+
+  return auth.currentUser.getIdToken();
+}
+
+export function getCurrentUserEmail() {
+  if (!auth.currentUser) {
+    return null;
+  }
+
+  return auth.currentUser.email;
+}
