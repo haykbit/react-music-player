93fab7863647320e50ad763bdcfa0fe95815a344
haykbit <hpetrosyan.projects@gmail.com>
2021-09-21 15:11:04 +0200

    fix: Playlist carrusel has been separated to a component

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import { RiSettings4Line, RiMusic2Line, RiHistoryFill } from "react-icons/ri";
 import { MdFavoriteBorder } from "react-icons/md";
 import { GoListUnordered } from "react-icons/go";
 import { FaUserCircle } from "react-icons/fa";
+import PlaylistCarrusel from "../PlaylistCarrusel";
 import userImage from "../../assets/images/profile.jpg";
 import portadaUno from "../../assets/images/portada-1.png";
 import portadaDos from "../../assets/images/portada-2.png";
@@ -66,102 +67,7 @@ function Navbar() {
         </div>
         <div className="menu-general"></div>
       </div>
-      <div className="recomend-container">
-        <h2 className="recomend-title">Te puede gustar...</h2>
-        <div className="recomend-list">
-          <div className="scroll">
-            <a href="#" className="fill-div">
-              <div
-                className="list-item"
-                style={{
-                  backgroundImage: `url(${portadaUno})`,
-                  backgroundSize: "auto",
-                  backgroundRepeat: "no-repeat",
-                }}
-              >
-                <div className="list-name">
-                  <h1>Lista Pop Rock España</h1>
-                </div>
-              </div>
-            </a>
-
-            <a href="#" className="fill-div">
-              <div
-                className="list-item"
-                style={{
-                  backgroundImage: `url(${portadaDos})`,
-                  backgroundSize: "auto",
-                  backgroundRepeat: "no-repeat",
-                }}
-              >
-                <div className="list-name">
-                  <h1>Lista Pop Rock España</h1>
-                </div>
-              </div>
-            </a>
-            <div
-              className="list-item"
-              style={{
-                backgroundImage: `url(${portadaTres})`,
-                backgroundSize: "auto",
-                backgroundRepeat: "no-repeat",
-              }}
-            >
-              <div className="list-name">
-                <h1>Lista Pop Rock España</h1>
-              </div>
-            </div>
-            <div
-              className="list-item"
-              style={{
-                backgroundImage: `url(${portadaCuatro})`,
-                backgroundSize: "auto",
-                backgroundRepeat: "no-repeat",
-              }}
-            >
-              <div className="list-name">
-                <h1>Lista Pop Rock España</h1>
-              </div>
-            </div>
-            <div
-              className="list-item"
-              style={{
-                backgroundImage: `url(${portadaUno})`,
-                backgroundSize: "auto",
-                backgroundRepeat: "no-repeat",
-              }}
-            >
-              <div className="list-name">
-                <h1>Lista Pop Rock España</h1>
-              </div>
-            </div>
-            <div
-              className="list-item"
-              style={{
-                backgroundImage: `url(${portadaDos})`,
-                backgroundSize: "auto",
-                backgroundRepeat: "no-repeat",
-              }}
-            >
-              <div className="list-name">
-                <h1>Lista Pop Rock España</h1>
-              </div>
-            </div>
-            <div
-              className="list-item"
-              style={{
-                backgroundImage: `url(${portadaTres})`,
-                backgroundSize: "auto",
-                backgroundRepeat: "no-repeat",
-              }}
-            >
-              <div className="list-name">
-                <h1>Lista Pop Rock España</h1>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <PlaylistCarrusel />
     </>
   );
 }
