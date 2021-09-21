7afa42e86c90c8bb1804df99362b05540f7acc95
김은영 <solaz0824@gmail.com>
2021-09-21 13:18:26 +0200

    feat: LoginForm component added

---
+++ client/src/components/LoginForm/LoginForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function LoginForm() {
+  return (
+    <div className="login-box">
+      <input></input>
+      <input></input>
+    </div>
+  );
+}
+
+export default LoginForm;
