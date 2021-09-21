fad4c62fe4b0c5a7c2c2064655adcf3529bebf38
haykbit <hpetrosyan.projects@gmail.com>
2021-09-21 16:45:38 +0200

    feat: Created components fro carrusel

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
 import PlaylistCarrusel from "../../components/PlaylistCarrusel";
 import Navbar from "../../components/Navbar/Navbar";
+import "./style/home.scss";
 
 function Home() {
   return (
     <>
       <Navbar />
+      <PlaylistCarrusel />
+      <PlaylistCarrusel />
+      <PlaylistCarrusel />
+      <PlaylistCarrusel />
       <PlaylistCarrusel />
     </>
   );
