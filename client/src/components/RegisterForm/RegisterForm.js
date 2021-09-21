3c1d8974d831ff7457c48c6c231a7e2a2936beae
Sebastian Brupbacher <sebastian.eliasb@gmail.com>
2021-09-21 16:32:16 +0200

    style: front-end register-page/first finish

--- client/src/components/RegisterForm/RegisterForm.js
+++ client/src/components/RegisterForm/RegisterForm.js
@@ -5,17 +5,30 @@ function RegisterForm() {
   return (
     <form>
       <div className="register-box">
-        <div className="text">
-          <p>Register</p>
+        <div className="text">Register</div>
+        <div></div>
+        <input className="register-inputs" placeholder="Name"></input>
+        <input className="register-inputs" placeholder="Password"></input>
+        <input className="register-inputs" placeholder="Surname"></input>
+        <input
+          className="register-inputs"
+          placeholder="Confirm password"
+        ></input>
+        <input className="register-inputs" placeholder="Email"></input>
+        <div className="checkboxes">
+          <label>
+            <input type="checkbox"></input>
+            <span>I accept privacy polices</span>
+          </label>
+          <label>
+            <input type="checkbox"></input>
+            <span>Subscribe to our newsletter</span>
+          </label>
         </div>
-        <div></div>
-        <input className="register-inputs"></input>
-        <input className="register-inputs"></input>
-        <input className="register-inputs"></input>
-        <input className="register-inputs"></input>
-        <input className="register-inputs"></input>
-        <div></div>
-        <div>Register Button</div>
+        <button className="register-inputs button">Register</button>
+        <div>
+          <p>* Your data will be saved on a secured server</p>
+        </div>
       </div>
     </form>
   );
