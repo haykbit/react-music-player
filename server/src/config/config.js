a755a874dc8762de745fbd6938c613e779ca7146
Jordi <aejordi@gmail.com>
2021-09-22 10:58:17 +0200

    feat: login controller and route created

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -9,10 +9,16 @@ const {
   NODE_ENV,
   PORT,
   CLIENT_URL,
-  ENCRYPTION_SALT_TEST,
-  ENCRYPTION_SALT_DEVELOPMENT,
-  ENCRYPTION_SALT_PRODUCTION,
-  ACCESS_TOKEN_SECRET,
+  FB_CERT_TYPE,
+  FB_CERT_PROJECT_ID,
+  FB_CERT_PRIVATE_KEY_ID,
+  FB_CERT_PRIVATE_KEY,
+  FB_CERT_CLIENT_EMAIL,
+  FB_CERT_CLIEND_ID,
+  FB_CERT_AUTH_URI,
+  FB_CERT_TOKEN_URI,
+  FB_CERT_AUTH_PROVIDER_X509_CERT_URL,
+  FB_CERT_CLIENT_X509_CERT_URL,
 } = process.env;
 
 const ENV = NODE_ENV || "development";
@@ -29,7 +35,18 @@ const CONFIG = {
       url: MONGO_DB_URL_PRODUCTION,
     },
     firebase: {
-      certConfig: {},
+      certConfig: {
+        type: FB_CERT_TYPE,
+        project_id: FB_CERT_PROJECT_ID,
+        private_key_id: FB_CERT_PRIVATE_KEY_ID,
+        private_key: FB_CERT_PRIVATE_KEY,
+        client_email: FB_CERT_CLIENT_EMAIL,
+        client_id: FB_CERT_CLIEND_ID,
+        aut_uri: FB_CERT_AUTH_URI,
+        token_uri: FB_CERT_TOKEN_URI,
+        auth_provider_x509_cert_url: FB_CERT_AUTH_PROVIDER_X509_CERT_URL,
+        client_x509_cert_url: FB_CERT_CLIENT_X509_CERT_URL,
+      },
     },
   },
 
@@ -44,7 +61,18 @@ const CONFIG = {
       url: MONGO_DB_URL_DEVELOPMENT,
     },
     firebase: {
-      certConfig: {},
+      certConfig: {
+        type: FB_CERT_TYPE,
+        project_id: FB_CERT_PROJECT_ID,
+        private_key_id: FB_CERT_PRIVATE_KEY_ID,
+        private_key: FB_CERT_PRIVATE_KEY,
+        client_email: FB_CERT_CLIENT_EMAIL,
+        client_id: FB_CERT_CLIEND_ID,
+        aut_uri: FB_CERT_AUTH_URI,
+        token_uri: FB_CERT_TOKEN_URI,
+        auth_provider_x509_cert_url: FB_CERT_AUTH_PROVIDER_X509_CERT_URL,
+        client_x509_cert_url: FB_CERT_CLIENT_X509_CERT_URL,
+      },
     },
   },
 
@@ -59,7 +87,18 @@ const CONFIG = {
       url: MONGO_DB_URL_TEST,
     },
     firebase: {
-      certConfig: {},
+      certConfig: {
+        type: FB_CERT_TYPE,
+        project_id: FB_CERT_PROJECT_ID,
+        private_key_id: FB_CERT_PRIVATE_KEY_ID,
+        private_key: FB_CERT_PRIVATE_KEY,
+        client_email: FB_CERT_CLIENT_EMAIL,
+        client_id: FB_CERT_CLIEND_ID,
+        aut_uri: FB_CERT_AUTH_URI,
+        token_uri: FB_CERT_TOKEN_URI,
+        auth_provider_x509_cert_url: FB_CERT_AUTH_PROVIDER_X509_CERT_URL,
+        client_x509_cert_url: FB_CERT_CLIENT_X509_CERT_URL,
+      },
     },
   },
 };
---
+++ server/src/middleware/index.js
@@ -0,0 +1,5 @@
+const authMiddleware = require("./authMiddleware");
+
+module.exports = {
+  authMiddleware,
+};
