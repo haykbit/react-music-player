df4da847f60e2c030187825f98c206a6e4e22dd1
Sebastian Brupbacher <sebastian.eliasb@gmail.com>
2021-09-22 15:45:31 +0200

    fix: Folder deletion

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,7 +3,6 @@ import PlaylistCarrusel from "../../components/PlaylistCarrusel";
 import PlaylistNew from "../../components/PlaylistNew/PlaylistNew";
 import Navbar from "../../components/Navbar/Navbar";
 import AlbumExplorer from "../../components/AlbumExplorer";
-import Example from "../../components/Slide/example";
 
 import "./style/home.scss";
 
@@ -14,7 +13,6 @@ function Home() {
       <PlaylistCarrusel />
       <PlaylistNew />
       <AlbumExplorer />
-      <Example />
     </>
   );
 }
