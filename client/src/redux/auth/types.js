ca0430f8a4e2c01c302e1435cf4f95b8d55999d2
김은영 <solaz0824@gmail.com>
2021-09-22 09:38:45 +0200

    fix: issue with label fixed

--- client/src/redux/auth/types.js
+++ client/src/redux/auth/types.js
@@ -2,3 +2,4 @@ export const LOGIN_REQUEST = "LOGIN_REQUEST";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAIL = "LOGIN_FAIL";
 export const LOAD_PROFILE = "LOAD_PROFILE";
+export const LOG_OUT = "LOG_OUT";
