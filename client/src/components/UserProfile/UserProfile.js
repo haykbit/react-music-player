53bdac50c00f74c74911871239d63b333917924b
haykbit <hpetrosyan.projects@gmail.com>
2021-09-23 14:41:28 +0200

    style: button style added

--- client/src/components/UserProfile/UserProfile.js
+++ client/src/components/UserProfile/UserProfile.js
@@ -11,12 +11,28 @@ function UserProfile() {
   return (
     <>
       <div className="profile-container">
-        <div className="profile-menu">
-          <ul>
-            <li></li>
-          </ul>
+        <h2 className="recomend-title">Profile information</h2>
+        <div className="profile-content">
+          <div className="lateral-menu">
+            <div className="profile-menu">
+              <ul>
+                <li>User information</li>
+                <li>Payment information</li>
+                <li>Subscription</li>
+                <li>Uploads</li>
+              </ul>
+            </div>
+            <div>
+              <button>Home</button>
+            </div>
+          </div>
+          <div className="profile-item">
+            <div className="user-info"></div>
+            <div className="payment-info"></div>
+            <div className="sub-info"></div>
+            <div className="upload-info"></div>
+          </div>
         </div>
-        <div className="profile-item"></div>
       </div>
     </>
   );
