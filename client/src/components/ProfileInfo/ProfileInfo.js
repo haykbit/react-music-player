ab24466cdb03ca164e4118f93346b91fd2dd8b92
haykbit <hpetrosyan.projects@gmail.com>
2021-09-25 19:06:07 +0200

    feat: User payment information card added

--- client/src/components/ProfileInfo/ProfileInfo.js
+++ client/src/components/ProfileInfo/ProfileInfo.js
@@ -7,141 +7,143 @@ import Input from "../Buttons/index";
 function ProfileInfo() {
   return (
     <>
-      <h2 className="user-info-title">User information</h2>
       <div className="user-info">
-        <div className="left-column">
-          <div className="profile-picture-box">
-            <div
-              className="profile-picture"
-              style={{
-                backgroundRepeat: "no-repeat",
-                backgroundImage: `url(${userImage})`,
-              }}
-            >
-              {" "}
+        <h2 className="user-info-title">User information</h2>
+        <div className="content">
+          <div className="left-column">
+            <div className="profile-picture-box">
+              <div
+                className="profile-picture"
+                style={{
+                  backgroundRepeat: "no-repeat",
+                  backgroundImage: `url(${userImage})`,
+                }}
+              >
+                {" "}
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="right-column">
-          <div className="form-box">
-            <form>
-              <div>
-                <input className="user-input" placeholder="Name" />
-                <input className="user-input" placeholder="Surname" />
+          <div className="right-column">
+            <div className="form-box">
+              <form>
+                <div>
+                  <input className="user-input" placeholder="Name" />
+                  <input className="user-input" placeholder="Surname" />
+                </div>
+
+                <div>
+                  <input className="user-input" placeholder="Email" />
+                  <Button className="user-input password-button">
+                    Reset Password
+                  </Button>
+                </div>
+              </form>
+            </div>
+            <div className="genre-box">
+              <div className="genre-side">
+                <div className="genre-checkbox-box">
+                  <label>
+                    Pop Rock
+                    <input className="input-checkbox" type="checkbox"></input>
+                  </label>
+                </div>
+                <div className="genre-checkbox-box">
+                  <label>
+                    Rock
+                    <input className="input-checkbox" type="checkbox"></input>
+                  </label>
+                </div>
+                <div className="genre-checkbox-box">
+                  <label>
+                    Reggaeton
+                    <input className="input-checkbox" type="checkbox"></input>
+                  </label>
+                </div>
+                <div className="genre-checkbox-box">
+                  <label>
+                    Indie
+                    <input className="input-checkbox" type="checkbox"></input>
+                  </label>
+                </div>
+                <div className="genre-checkbox-box">
+                  <label>
+                    EDM
+                    <input className="input-checkbox" type="checkbox"></input>
+                  </label>
+                </div>
+                <div className="genre-checkbox-box">
+                  <label>
+                    Rap
+                    <input className="input-checkbox" type="checkbox"></input>
+                  </label>
+                </div>
+                <div className="genre-checkbox-box">
+                  <label>
+                    Metal
+                    <input className="input-checkbox" type="checkbox"></input>
+                  </label>
+                </div>
+                <div className="genre-checkbox-box">
+                  <label>
+                    Techno
+                    <input className="input-checkbox" type="checkbox"></input>
+                  </label>
+                </div>
+                <div className="genre-checkbox-box">
+                  <label>
+                    Jazz
+                    <input className="input-checkbox" type="checkbox"></input>
+                  </label>
+                </div>
+                <div className="genre-checkbox-box">
+                  <label>
+                    Blues
+                    <input className="input-checkbox" type="checkbox"></input>
+                  </label>
+                </div>
+                <div className="genre-checkbox-box">
+                  <label>
+                    Country
+                    <input className="input-checkbox" type="checkbox"></input>
+                  </label>
+                </div>
+                <div className="genre-checkbox-box">
+                  <label>
+                    Country
+                    <input className="input-checkbox" type="checkbox"></input>
+                  </label>
+                </div>
+                <div className="genre-checkbox-box">
+                  <label>
+                    Country
+                    <input className="input-checkbox" type="checkbox"></input>
+                  </label>
+                </div>
+                <div className="genre-checkbox-box">
+                  <label>
+                    Country
+                    <input className="input-checkbox" type="checkbox"></input>
+                  </label>
+                </div>
+                <div className="genre-checkbox-box">
+                  <label>
+                    Country
+                    <input className="input-checkbox" type="checkbox"></input>
+                  </label>
+                </div>
+                <div className="genre-checkbox-box">
+                  <label>
+                    Country
+                    <input className="input-checkbox" type="checkbox"></input>
+                  </label>
+                </div>
               </div>
-
-              <div>
-                <input className="user-input" placeholder="Email" />
-                <Button className="user-input password-button">
-                  Reset Password
+              <div className="button-side">
+                <Button className="edit-btn" label="Edit">
+                  Edit
                 </Button>
               </div>
-            </form>
-          </div>
-          <div className="genre-box">
-            <div className="genre-side">
-              <div className="genre-checkbox-box">
-                <label>
-                  Pop Rock
-                  <input className="input-checkbox" type="checkbox"></input>
-                </label>
-              </div>
-              <div className="genre-checkbox-box">
-                <label>
-                  Rock
-                  <input className="input-checkbox" type="checkbox"></input>
-                </label>
-              </div>
-              <div className="genre-checkbox-box">
-                <label>
-                  Reggaeton
-                  <input className="input-checkbox" type="checkbox"></input>
-                </label>
-              </div>
-              <div className="genre-checkbox-box">
-                <label>
-                  Indie
-                  <input className="input-checkbox" type="checkbox"></input>
-                </label>
-              </div>
-              <div className="genre-checkbox-box">
-                <label>
-                  EDM
-                  <input className="input-checkbox" type="checkbox"></input>
-                </label>
-              </div>
-              <div className="genre-checkbox-box">
-                <label>
-                  Rap
-                  <input className="input-checkbox" type="checkbox"></input>
-                </label>
-              </div>
-              <div className="genre-checkbox-box">
-                <label>
-                  Metal
-                  <input className="input-checkbox" type="checkbox"></input>
-                </label>
-              </div>
-              <div className="genre-checkbox-box">
-                <label>
-                  Techno
-                  <input className="input-checkbox" type="checkbox"></input>
-                </label>
-              </div>
-              <div className="genre-checkbox-box">
-                <label>
-                  Jazz
-                  <input className="input-checkbox" type="checkbox"></input>
-                </label>
-              </div>
-              <div className="genre-checkbox-box">
-                <label>
-                  Blues
-                  <input className="input-checkbox" type="checkbox"></input>
-                </label>
-              </div>
-              <div className="genre-checkbox-box">
-                <label>
-                  Country
-                  <input className="input-checkbox" type="checkbox"></input>
-                </label>
-              </div>
-              <div className="genre-checkbox-box">
-                <label>
-                  Country
-                  <input className="input-checkbox" type="checkbox"></input>
-                </label>
-              </div>
-              <div className="genre-checkbox-box">
-                <label>
-                  Country
-                  <input className="input-checkbox" type="checkbox"></input>
-                </label>
-              </div>
-              <div className="genre-checkbox-box">
-                <label>
-                  Country
-                  <input className="input-checkbox" type="checkbox"></input>
-                </label>
-              </div>
-              <div className="genre-checkbox-box">
-                <label>
-                  Country
-                  <input className="input-checkbox" type="checkbox"></input>
-                </label>
-              </div>
-              <div className="genre-checkbox-box">
-                <label>
-                  Country
-                  <input className="input-checkbox" type="checkbox"></input>
-                </label>
-              </div>
-            </div>
-            <div className="button-side">
-              <Button className="edit-btn" label="Edit">
-                Edit
-              </Button>
             </div>
           </div>
         </div>
