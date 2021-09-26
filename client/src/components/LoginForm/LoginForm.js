6adf1fe07e5a26e5d2ed2c85c0b3709771aa9584
Jordi <aejordi@gmail.com>
2021-09-26 19:33:09 +0200

    feat: pass recover menu frontend

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -6,9 +6,11 @@ import { login, loginWithEmailAndPassword } from "../../redux/auth/action";
 import google from "../../assets/images/search.png";
 import gmail from "../../assets/images/gmail.png";
 import "./style/loginform.scss";
+import PasswordRecovery from "../PasswordRecovery/PasswordRecovery";
 
 function LoginForm() {
   const [loginForm, setLoginForm] = useState({ email: "", password: "" });
+  const [recoverPass, setRecoverPass] = useState({ recover: false });
   const { email, password } = loginForm;
   const dispatch = useDispatch();
 
@@ -24,38 +26,55 @@ function LoginForm() {
     dispatch(loginWithEmailAndPassword(email, password));
   };
 
+  const handleRecover = (event) => {
+    setRecoverPass({ recover: true });
+  };
+
+  const recoverOff = () => {
+    setRecoverPass({ recover: false });
+  };
+
   return (
     <div className="login-container" onSubmit={handleLoginWithEmailAndPassword}>
       <div className="login-section">
-        <form className="login-form">
-          <h2>Login</h2>
-          <input
-            placeholder="Email"
-            value={email}
-            name="email"
-            onChange={handleChange}
-          />
-          <input
-            placeholder="Password"
-            value={password}
-            name="password"
-            onChange={handleChange}
-          />
-          <button className="login-btn">Login</button>
-        </form>
-        <div className="forgotPassword">
-          Forgot your password? Click <Link to="/recover">here</Link>
-        </div>
+        {recoverPass.recover ? (
+          <PasswordRecovery recoverOff={recoverOff} />
+        ) : (
+          <>
+            <form className="login-form">
+              <h2>Login</h2>
+              <input
+                placeholder="Email"
+                value={email}
+                name="email"
+                onChange={handleChange}
+              />
+              <input
+                placeholder="Password"
+                value={password}
+                name="password"
+                onChange={handleChange}
+              />
+              <button className="login-btn">Login</button>
+            </form>
+            <div className="forgotPassword">
+              Forgot your password? Click{" "}
+              <span className="recover-link" onClick={handleRecover}>
+                here
+              </span>
+            </div>
 
-        <h3>Or</h3>
-        <button className="login-gl-btn" onClick={handleLoginWithGoogle}>
-          <img src={google} alt="" />
-          Login with Google
-        </button>
-        <button className="register-btn">
-          <img src={gmail} alt="" />
-          Register
-        </button>
+            <h3>Or</h3>
+            <button className="login-gl-btn" onClick={handleLoginWithGoogle}>
+              <img src={google} alt="" />
+              Login with Google
+            </button>
+            <button className="register-btn">
+              <img src={gmail} alt="" />
+              Register
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
