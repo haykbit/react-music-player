c435c2b2c83d831041d3b48928f87bca30671e60
Jordi <aejordi@gmail.com>
2021-09-26 14:21:34 +0200

    fix: solved login req.body.user error

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -2,7 +2,7 @@ const db = require("../models");
 
 async function signIn(req, res, next) {
   const { uid, email } = req.user;
-  const { firstName, lastName } = req.body.user;
+  //const { firstName, lastName } = req.body.user;
   try {
     const user = await db.User.findOne({ email: email });
 
@@ -13,8 +13,8 @@ async function signIn(req, res, next) {
     const newUser = await db.User.create({
       firebase_id: uid,
       email: email,
-      firstName: firstName || "",
-      lastName: lastName || "",
+      firstName: req.body.user ? req.body.user.firstName : "",
+      lastName: req.body.user ? req.body.user.lastName : "",
     });
 
     res.sendStatus(201);
