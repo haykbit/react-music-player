277d65c0b0469c577fa49edd4d43437f6d65cc08
Jordi <aejordi@gmail.com>
2021-09-28 18:58:00 +0200

    feat: redux for song structure

---
+++ client/src/redux/song/action.js
@@ -0,0 +1,13 @@
+import {
+  POST_SONG_REQUEST,
+  POST_SONG_SUCCESS,
+  POST_SONG_FAIL,
+  GET_SONG_REQUEST,
+  GET_SONG_SUCCESS,
+  GET_SONG_FAIL,
+  UPDATE_SONG_REQUEST,
+  UPDATE_SONG_SUCCESS,
+  UPDATE_SONG_FAIL,
+} from "./types";
+
+export const uploadSong = () => async (dispatch) => {};
