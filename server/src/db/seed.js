a7ee845fe17bde28dc810c5a3ccb0cf6adc1ebdd
imalovelyghost <imalovelyghost13@gmail.com>
2021-09-28 11:06:23 +0200

    feat: song seeder included into main seeder

--- server/src/db/seed.js
+++ server/src/db/seed.js
@@ -22,18 +22,51 @@ async function seedUsers() {
   }
 }
 
+async function seedSongs() {
+  try {
+    db.Song.insertMany([
+      {
+        name : "SongNAme",
+        description : "It talks about dady issues",
+        ratings : 4.5,
+        images : [{
+            public_id : "products/dsvbpny402gelwugv2le",
+            url : "https://res.cloudinary.com/bookit/image/upload/v1608062030/products/dsvbpny402gelwugv2le.jpg"
+        }],
+        category : "K-pop",
+        author : "Pepe Author",
+        numOfReviews : 32,
+        reviews: []
+    }
+
+    ]);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+
 async function seedDatabase() {
   try {
     const userExists = await db.User.findOne({
       firebase_id: "I3wKx6kfyXZykQ77pAAMQK3vQ7s1",
     }).lean();
+    const songExists = await db.Sing.findOne({ id:"_id"}).lean();
+
+
     if (!userExists) {
       await mongoose.connection.collection("users").drop();
       seedUsers();
+    }
+    if (!songExists) {
+      await mongoose.connection.collection("songs").drop();
+      seedSongs();
     }
   } catch (err) {
     console.log(err);
   }
 }
 
+
 module.exports = seedDatabase;
+
