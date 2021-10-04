70e2e3ec1bb4b4eee0080d2503fd135ea5947863
Sebastian Brupbacher <sebastian.eliasb@gmail.com>
2021-10-04 15:28:26 +0200

    feat: Individual Songs component

---
+++ client/src/components/IndivualSong/IndividualSong.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { BsFillCaretRightFill } from "react-icons/bs";
+import { FaRegHeart } from "react-icons/fa";
+
+function IndividualSong() {
+  return (
+    <div className="song-item-playlist">
+      <div className="song-info">
+        <div className="song-cover"></div>
+        <div className="song-name">
+          <h3>Coldplay</h3>
+          <h5>Noches en vela</h5>
+        </div>
+      </div>
+
+      <div className="song-actions">
+        <div className="song-play">
+          <button>
+            <BsFillCaretRightFill className="play-icon" />
+          </button>
+        </div>
+        <div className="song-time">
+          <h4>4:23</h4>
+        </div>
+        <div className="song-like">
+          <button>
+            <FaRegHeart className="like-icon" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default IndividualSong;
