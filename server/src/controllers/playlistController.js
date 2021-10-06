98392073e61693c1ee739f5c891ac7d66bfd2b4d
imalovelyghost <imalovelyghost13@gmail.com>
2021-10-06 18:13:37 +0200

    feat:init of this branch

--- server/src/controllers/playlistController.js
+++ server/src/controllers/playlistController.js
@@ -61,10 +61,52 @@ async function updatePlaylist(req, res, next) {
     console.log(err);
   }
 }
+async function addSong(req, res, next) {
+  // const { id: songId } = req.params;
+  // const { id: playlistId } = req.params;
+  // const { userId } = req.body;
+  console.log("Hiiddd");
+
+  // try {
+  //   const checkSong = await db.Song.findById(songId);
+  //   const checkUser = await db.User.findOne({ firebase_id: userId });
+  //   const checkSong = await db.Song.findById(songId);
+
+  //   if (
+  //     !checkSong.likedBy.includes(userId) &&
+  //     !checkUser.myFavoriteSongs.includes(songId)
+  //   ) {
+  //     const song = await db.Song.findOneAndUpdate(
+  //       { _id: songId },
+  //       {
+  //         $inc: {
+  //           likes: 1,
+  //         },
+  //         $push: {
+  //           likedBy: userId,
+  //         },
+  //       }
+  //     );
+  //     await db.User.findOneAndUpdate(
+  //       { firebase_id: userId },
+  //       {
+  //         $push: { myFavoriteSongs: [{ _id: songId }] },
+  //       }
+  //     );
+  //   }
+
+  //   res.status(200).send({
+  //     message: "OK",
+  //   });
+  // } catch (error) {
+  //   next(error);
+  // }
+}
 
 module.exports = {
   fetchPlaylists: fetchPlaylists,
   getPlaylistById: getPlaylistById,
   removePlaylistById: removePlaylistById,
   updatePlaylist: updatePlaylist,
+  addSong: addSong,
 };
