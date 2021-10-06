055d03c3a887b2d56d7a97b52553638c72d68ec2
Jordi <aejordi@gmail.com>
2021-10-06 12:55:00 +0200

    feat: fixed seeder and created Redux structure for Player

---
+++ client/src/redux/player/state.js
@@ -0,0 +1,10 @@
+const INITIAL_STATE = {
+  songData: null,
+  loading: false,
+  error: null,
+  playSuccess: false,
+  playNextSuccess: false,
+  playPrevSuccess: false,
+};
+
+export default INITIAL_STATE;
