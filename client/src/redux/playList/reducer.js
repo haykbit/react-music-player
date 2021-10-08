18e2f399ead32e950bdc0b4267212347775fd3a6
김은영 <solaz0824@gmail.com>
2021-10-08 16:21:29 +0200

    fix: playlist, song and user models changed

--- client/src/redux/playList/reducer.js
+++ client/src/redux/playList/reducer.js
@@ -1,4 +1,4 @@
-import PLAYLST_STATE from "./state";
+import INITIAL_STATE from "./state";
 import {
   PLAYLIST_CREATE_REQUEST,
   PLAYLIST_CREATE_SUCCESS,
@@ -14,7 +14,7 @@ import {
   ADD_SONG_TO_PLAYLIST_FAIL,
 } from "./types";
 
-const reducer = (state = PLAYLIST_STATE, action) => {
+const reducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case PLAYLIST_CREATE_REQUEST:
       return {
@@ -29,6 +29,13 @@ const reducer = (state = PLAYLIST_STATE, action) => {
         addedToPlaylistSuccess: false,
         myPlaylistCreatedSuccess: false,
         error: null,
+      };
+    case PLAYLIST_CREATE_FAIL:
+      return {
+        playlistCreatedSuccess: false,
+        addedToPlaylistSuccess: false,
+        myPlaylistCreatedSuccess: false,
+        error: action.payload,
       };
   }
 };
