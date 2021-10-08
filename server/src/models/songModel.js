ffe6b5b71e2889dc81e6f2c459a4820013bdca7e
Eunyoung Kim <50446830+solaz0824@users.noreply.github.com>
2021-10-08 19:14:19 +0200

    F54/playlist creation (#70)
    * fix: file name
    
    * fix: rename playlist
    
    * fix: duplicated code removed and homepage issue fixed
    
    * fix: playlist, song and user models changed
    
    * fix: playlist, song and user models changed
    
    * feat: basic structure for playlist creation done
    
    * fix: unused files removed
    
    * fix: seed commented
    
    Co-authored-by: Jordi Arnau <75805206+JordixArnau@users.noreply.github.com>

--- server/src/models/songModel.js
+++ server/src/models/songModel.js
@@ -1,91 +1,87 @@
 const mongoose = require("mongoose");
 const { Schema } = require("mongoose");
 
-const SongSchema = new Schema({
-  title: {
-    type: String,
-    required: [true, "Please enter title song"],
-    trim: true,
-    maxLength: [30, "Song title cannot exceed 30 characters"],
-  },
-  artist: {
-    type: String,
-    required: [true, "Please enter artist song"],
-    trim: true,
-    maxLength: [30, "Song artist cannot exceed 30 characters"],
-  },
-  genre: {
-    type: String,
-    required: [true, "Please select genre for this song"],
-    enum: {
-      values: [
-        "Country",
-        "Electronic dance music (EDM)",
-        "Hip-hop",
-        "Indie rock",
-        "Jazz",
-        "K-pop",
-        "Metal",
-        "Oldies",
-        "Pop",
-        "Rap",
-        "Rhythm & blues (R&B)",
-        "Rock",
-        "Techno",
-        "Folk",
-        "Ska",
-        "Reggae",
-        "Punk",
-      ],
-      message: "Please select correct genre for song",
+const SongSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Please enter title song"],
+      trim: true,
+      maxLength: [30, "Song title cannot exceed 30 characters"],
+    },
+    artist: {
+      type: String,
+      required: [true, "Please enter artist song"],
+      trim: true,
+      maxLength: [30, "Song artist cannot exceed 30 characters"],
+    },
+    genre: {
+      type: String,
+      required: [true, "Please select genre for this song"],
+      enum: {
+        values: [
+          "Country",
+          "Electronic dance music (EDM)",
+          "Hip-hop",
+          "Indie rock",
+          "Jazz",
+          "K-pop",
+          "Metal",
+          "Oldies",
+          "Pop",
+          "Rap",
+          "Rhythm & blues (R&B)",
+          "Rock",
+          "Techno",
+          "Folk",
+          "Ska",
+          "Reggae",
+          "Punk",
+        ],
+        message: "Please select correct genre for song",
+      },
+    },
+    duration: {
+      type: Number,
+      default: 0,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+    album: {
+      type: String,
+    },
+    // album: {
+    //   type: mongoose.Schema.ObjectId,
+    //   ref: "Album",
+    //   required: false,
+    // },
+    // private: {
+    //   type: Boolean,
+    //   required: true,
+    // },
+    owner: {
+      type: String,
+      ref: "User",
+      required: true,
+    },
+
+    likes: {
+      type: Number,
+      default: 0,
+    },
+    likedBy: {
+      // type: [{ type: Schema.Types.ObjectId, ref: "user" }],
+      type: Array,
+      default: [],
+    },
+    played: {
+      type: Number,
     },
   },
-  duration: {
-    type: Number,
-    default: 0,
-  },
-  url: {
-    type: String,
-    required: true,
-  },
-  album: {
-    type: String,
-  },
-  // album: {
-  //   type: mongoose.Schema.ObjectId,
-  //   ref: "Album",
-  //   required: false,
-  // },
-  // private: {
-  //   type: Boolean,
-  //   required: true,
-  // },
-  owner: {
-    type: String,
-    ref: "User",
-    required: true,
-  },
-  popularity: {
-    type: Number,
-    default: 0,
-  },
-
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  likes: {
-    type: Number,
-  },
-  likedBy: {
-    // type: [{ type: Schema.Types.ObjectId, ref: "user" }],
-    type: Array,
-    default: [],
-  },
-  played: {
-    type: Number,
-  },
-});
+  { timestamp: true }
+);
 
 const Song = mongoose.model("Song", SongSchema);
 module.exports = {
