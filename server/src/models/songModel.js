97d80704a62d9e8db9fb9cafd26db7ce2e2de001
김은영 <solaz0824@gmail.com>
2021-10-08 11:23:40 +0200

    fix: song model fixed

--- server/src/models/songModel.js
+++ server/src/models/songModel.js
@@ -56,12 +56,13 @@ const SongSchema = new Schema({
   //   ref: "Album",
   //   required: false,
   // },
-  private: {
-    type: Boolean,
-    required: true,
-  },
+  // private: {
+  //   type: Boolean,
+  //   required: true,
+  // },
   owner: {
-    type: mongoose.Schema.ObjectId,
+    // type: mongoose.Schema.ObjectId,
+    type: String,
     ref: "User",
     required: true,
   },
@@ -83,7 +84,7 @@ const SongSchema = new Schema({
     type: Number,
   },
   likedBy: {
-    type: [{ type: Schema.Types.ObjectId, ref: "user" }],
+    // type: [{ type: Schema.Types.ObjectId, ref: "user" }],
     type: Array,
     default: [],
   },
