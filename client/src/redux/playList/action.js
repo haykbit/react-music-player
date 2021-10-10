196adaf77ee609442704fa50ed3de1a45eb9d6d1
김은영 <solaz0824@gmail.com>
2021-10-10 14:42:49 +0200

    feat: get my playlist basic stucture 2

--- client/src/redux/playList/action.js
+++ client/src/redux/playList/action.js
@@ -4,6 +4,9 @@ import {
   PLAYLIST_CREATE_REQUEST,
   PLAYLIST_CREATE_SUCCESS,
   PLAYLIST_CREATE_FAIL,
+  GET_MY_PLAYLISTS_REQUEST,
+  GET_MY_PLAYLISTS_SUCCESS,
+  GET_MY_PLAYLISTS_FAIL,
 } from "./types";
 
 export const createNewPlaylist = (playlistData, image) => async (dispatch) => {
@@ -18,5 +21,13 @@ export const createNewPlaylist = (playlistData, image) => async (dispatch) => {
     await createPlaylists(playlist);
   } catch (error) {
     dispatch({ type: PLAYLIST_CREATE_FAIL, payload: error.message });
+  }
+};
+
+export const getMyPlaylists = (id) => async (dispatch) => {
+  dispatch({ type: GET_MY_PLAYLISTS_REQUEST });
+  try {
+  } catch (error) {
+    dispatch({ type: GET_MY_PLAYLISTS_FAIL, payload: error.message });
   }
 };
