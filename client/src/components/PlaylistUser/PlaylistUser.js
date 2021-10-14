7721ec168a1523db444fe73130f26b6ad85a2e26
Sebastian Brupbacher <sebastian.eliasb@gmail.com>
2021-10-14 21:28:03 +0200

    style: beginning of profile page

--- client/src/components/PlaylistUser/PlaylistUser.js
+++ client/src/components/PlaylistUser/PlaylistUser.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./style/playlistuser.scss";
 function PlaylistUser({ playlistUserData }) {
   return (
-    <div className="profile-container">
+    <div className="profile-page-container">
       <h2 className="profile-title">Profile information</h2>
       <h3 className="profile-name">
         {playlistUserData.firstName} {playlistUserData.lastName}
