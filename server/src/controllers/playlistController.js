b3acc5f428e5cac37086418b83b2c2ba081e49cf
imalovelyghost <imalovelyghost13@gmail.com>
2021-10-14 17:00:28 +0200

    add song tested and working

--- server/src/controllers/playlistController.js
+++ server/src/controllers/playlistController.js
@@ -106,29 +106,25 @@ async function updatePlaylist(req, res, next) {
   }
 }
 
+//To test
 async function addSongFromPlaylistView(req, res, next) {
   const { id: songId } = req.params;
-  const { myPlaylists } = req.body;
-  const { owner } = req.body;
+  const { playlistId } = req.body;
+  const { userId } = req.body;
 
   try {
-    // Encontrar la id del propietario de la playlist
-    const checkOwner = await db.Playlist.findById(owner);
+    const checkOwner = await db.Playlist.findById(playlistId, "owner");
 
-    // Encontrar la id de una playlist de la cual soy propietario
-    const checkPlaylist = await db.User.findById(myPlaylists);
-
-    // Encontrar cualquier cancion por su id
-    const checkSong = await db.Song.findById(songId);
+    const checkSong = await db.Playlist.find({
+      _id: playlistId,
+      songs: { $in: [songId] },
+    });
 
     if (
-      //Que el propietario de la playlist se incluya dentro de usuario como owner o user id
-      !checkOwner.user.includes(owner) &&
-      //Que el propietario de la playlist se incluya dentro  de usuario como una playlist que le pertenece
-      !checkPlaylist.user.includes(myPlaylists) &&
-      //Que cualquier cancion se incluyda dentro de playlist segun su id (si se repite NO)
-      !checkSong.playlist.includes(songId)
-    ) {
+      checkOwner.owner === userId
+      // && !checkSong
+    )
+      //{
       await db.Playlist.findOneAndUpdate(
         { _id: playlistId },
         {
@@ -137,10 +133,12 @@ async function addSongFromPlaylistView(req, res, next) {
           },
         }
       );
-      res.status(200).send({
-        message: "OK",
-      });
-    }
+    res.status(200).send({
+      message: "OK",
+    });
+    // } else {
+    //   res.status(error);
+    // }
   } catch (error) {
     next(error);
   }
