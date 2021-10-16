f3cfb6e0b9d4354e4fddb5c8c78131bb51b3aa87
Sebastian Brupbacher <sebastian.eliasb@gmail.com>
2021-10-16 15:35:25 +0200

    style: adding editIcon

--- client/src/components/PlaylistUser/PlaylistUser.js
+++ client/src/components/PlaylistUser/PlaylistUser.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style/playlistuser.scss";
 import backgroundPicture from "../../assets/images/background/profile-picture-background.jpg";
+import editIcon from "../../assets/images/icons/editIcon.png";
 function PlaylistUser({ playlistUserData }) {
   console.log(playlistUserData);
+
+  const [isUploaded, setIsUploaded] = useState(false);
+  const [image, setImage] = useState("");
+
+  function handleImageChange(e) {
+    if (e.target.files && e.target.files[0]) {
+      let reader = new FileReader();
+      setImage(e.target.files[0]);
+      reader.onload = function (e) {
+        setIsUploaded(true);
+      };
+      reader.readAsDataURL(e.target.files[0]);
+    }
+  }
   return (
     <div
       className="top-half"
@@ -10,6 +25,24 @@ function PlaylistUser({ playlistUserData }) {
         backgroundImage: `url(${backgroundPicture})`,
       }}
     >
+      <div className="edit-background">
+        {" "}
+        <label
+          htmlFor="upload-input"
+          className="edit-icon"
+          style={{
+            backgroundImage: `url(${editIcon})`,
+          }}
+        ></label>
+        <input
+          id="upload-input"
+          className="input-upload"
+          type="file"
+          accept=".jpg, .jpeg, .png"
+          onChange={handleImageChange}
+        />
+      </div>
+
       <div className="profile-page-container">
         <div className="profile-page-box">
           <div className="box-header">
