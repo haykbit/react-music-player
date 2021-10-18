b0daef32216641a826be03a28295884a4855a9de
김은영 <solaz0824@gmail.com>
2021-10-18 10:46:27 +0200

    fix: a comment removed

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -84,6 +84,7 @@ export async function cancelLikeSong(songId, userId) {
     },
   });
 }
+
 export async function getLikedSongs(id) {
   //get user id argument
   const userToken = await getCurrentUserToken();
@@ -168,7 +169,6 @@ export async function getMyPlaylistsList(id) {
   });
 }
 
-//!! it is the same as getMySongsData function.
 export async function getMySongsPlaylist(userId) {
   const userToken = await getCurrentUserToken();
   return axios({
@@ -180,7 +180,6 @@ export async function getMySongsPlaylist(userId) {
   });
 }
 
-//In progress - To ask kym
 export async function addSongToPlaylist(playlistId, userId, songId) {
   const userToken = await getCurrentUserToken();
   return axios({
@@ -192,7 +191,6 @@ export async function addSongToPlaylist(playlistId, userId, songId) {
     },
   });
 }
-//
 
 export async function countPlayed(songId) {
   const userToken = await getCurrentUserToken();
