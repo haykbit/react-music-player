0ac305f27e104096cc6117f9569e80c1263ec001
Jordi <aejordi@gmail.com>
2021-10-20 00:55:49 +0200

    fix: removed unnecessary fields and private songs from seeders

--- statServer/database/seeders/SongSeeder.php
+++ statServer/database/seeders/SongSeeder.php
@@ -19,13 +19,10 @@ class SongSeeder extends Seeder
                 ["original_id" => "6169963e6f4479521c3b4672", "artist" => "CxYYshpGWbeEy7zdgjd6ckrWBNZ2", "genre" => "Rap", "likes" => 13, "played" => 14, "created_at" => Carbon::now(), "updated_at" => Carbon::now()],
                 ["original_id" => "6169963e6f4479521c3b4673", "artist" => "CxYYshpGWbeEy7zdgjd6ckrWBNZ2", "genre" => "Rap", "likes" => 2, "played" => 53, "created_at" => Carbon::now(), "updated_at" => Carbon::now()],
                 ["original_id" => "6169963e6f4479521c3b4674", "artist" => "CxYYshpGWbeEy7zdgjd6ckrWBNZ2", "genre" => "Rap", "likes" => 89, "played" => 67, "created_at" => Carbon::now(), "updated_at" => Carbon::now()],
-                ["original_id" => "6169963e6f4479521c3b4675", "artist" => "CxYYshpGWbeEy7zdgjd6ckrWBNZ2", "genre" => "Rap", "likes" => 0, "played" => 2, "created_at" => Carbon::now(), "updated_at" => Carbon::now()],
-                ["original_id" => "6169963e6f4479521c3b4676", "artist" => "CxYYshpGWbeEy7zdgjd6ckrWBNZ2", "genre" => "Rap", "likes" => 3, "played" => 5, "created_at" => Carbon::now(), "updated_at" => Carbon::now()],
                 ["original_id" => "6169963e6f4479521c3b4677", "artist" => "zuvAukoB7ogMj7DHaimzKqEFI3C3", "genre" => "Ska", "likes" => 104, "played" => 241, "created_at" => Carbon::now(), "updated_at" => Carbon::now()],
                 ["original_id" => "6169963e6f4479521c3b4678", "artist" => "zuvAukoB7ogMj7DHaimzKqEFI3C3", "genre" => "Ska", "likes" => 25, "played" => 92, "created_at" => Carbon::now(), "updated_at" => Carbon::now()],
                 ["original_id" => "6169963e6f4479521c3b4679", "artist" => "zuvAukoB7ogMj7DHaimzKqEFI3C3", "genre" => "Ska", "likes" => 2, "played" => 10, "created_at" => Carbon::now(), "updated_at" => Carbon::now()],
                 ["original_id" => "6169963e6f4479521c3b467a", "artist" => "zuvAukoB7ogMj7DHaimzKqEFI3C3", "genre" => "Ska", "likes" => 401, "played" => 549, "created_at" => Carbon::now(), "updated_at" => Carbon::now()],
-                ["original_id" => "6169963e6f4479521c3b467b", "artist" => "zuvAukoB7ogMj7DHaimzKqEFI3C3", "genre" => "Ska", "likes" => 13, "played" => 14, "created_at" => Carbon::now(), "updated_at" => Carbon::now()],
             ];
             Song::insert($songs);
         }
