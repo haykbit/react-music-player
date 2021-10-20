e3773100d831b25cf124c1af451a43a342ac6a4a
Hayk Petrosyan Elmayan <80252196+haykbit@users.noreply.github.com>
2021-10-20 14:15:21 +0200

    F99/home finishing (#119)
    * feat: added functionality to render playlists from server
    
    * feat: render songs in home compontent
    
    * feat: added individualSong component on home playlist
    
    * fix: audio player width

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { BsFillCaretRightFill } from "react-icons/bs";
+
+import { getPublicSongs, getSongsFromPlaylist } from "../../api/api";
 import PlaylistCarrusel from "../../components/PlaylistCarrusel";
 import GeneralList from "../../components/GeneralList/GeneralList";
 import Navbar from "../../components/Navbar/Navbar";
@@ -7,11 +11,31 @@ import AlbumExplorer from "../../components/AlbumExplorer";
 import "./style/home.scss";
 
 function Home() {
+  const { loading, authObserverSuccess } = useSelector((state) => state.auth);
+  const [songs, setSongs] = useState(null);
+  const [generalSongs, setGeneralSongs] = useState(null);
+
+  async function getSongsData() {
+    const publicSongs = await getPublicSongs();
+    setSongs(publicSongs["data"]["data"]);
+  }
+
+  async function getGeneralSongsData() {
+    const songs = await getSongsFromPlaylist("61698eef6f4479521c3b4627");
+    setGeneralSongs(songs.data.data);
+  }
+
+  useEffect(() => {
+    if (!loading && authObserverSuccess) {
+      getSongsData();
+      getGeneralSongsData();
+    }
+  }, [loading, authObserverSuccess]);
   return (
     <>
       <Navbar />
       <PlaylistCarrusel />
-      <GeneralList />
+      <GeneralList playlist={songs} playlistGeneral={generalSongs} />
       <AlbumExplorer />
     </>
   );
