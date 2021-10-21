fe24b2ffe859cf5f84804c4a6d2b2b3e5624b81a
haykbit <hpetrosyan.projects@gmail.com>
2021-10-21 12:29:18 +0200

    fix: import error

--- client/src/pages/Artist/Artist.js
+++ client/src/pages/Artist/Artist.js
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from "react";
 import PlaylistCarrusel from "../../components/PlaylistCarrusel";
 import GeneralList from "../../components/GeneralList/GeneralList";
 import Navbar from "../../components/Navbar/Navbar";
-import AlbumExplorer from "../../components/AlbumExplorer";
-// import SongBar from "../../components/SongBar";
 
 import "./style/artist.scss";
 
